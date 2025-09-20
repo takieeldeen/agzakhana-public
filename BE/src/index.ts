81a195a72fff868561a3de78769fcf262a74ad36
takieeldeen <takie.eldeen1998@gmail.com>
2025-09-20 18:42:07 +0300

    Getting Payment key from paymob

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
-import app from "./app";
 import { config } from "dotenv";
 config();
+import mongoose from "mongoose";
+import app from "./app";
 
 const CONNECTION_STRING = process.env.DB_STRING?.replace(
   "<<USERNAME>>",
